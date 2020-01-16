ea1ea6b9dda645d3195605a7e58f8db4ca6ac91c
VukasinVujic <vukasinvujic021@gmail.com>
2020-01-16 11:36:04 +0100

    adding a search box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
+import { SearchBarComponent } from './layouts/header/search-bar/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
